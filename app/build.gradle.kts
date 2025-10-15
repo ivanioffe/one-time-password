0e4be7c89775a576684e2d690b91e716679e9cf5
Ivan Ioffe <iiooffee0202@gmail.com>
2025-10-15 19:42:08 +0300

    refactor(app): remove unnecessary dependencies and tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    testImplementation(libs.junit)
-
-    androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
